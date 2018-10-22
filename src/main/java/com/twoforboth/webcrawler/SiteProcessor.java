0c98be3ba4f0240d880f59a588d828edf2a9869b
Stuart Blavatnik <stuartb@twoforboth.com>
2018-10-21 22:09:16 -0400

    pseudo code

---
+++ src/main/java/com/twoforboth/webcrawler/SiteProcessor.java
@@ -0,0 +1,13 @@
+package com.twoforboth.webcrawler;
+
+public class SiteProcessor {
+
+    // Thoughts
+    // Given a URL
+    // parse pagedata
+    // keep a hash of each url -- key is the url -- value is the sublinks
+    // indicate if traversed to -- to prevent multiple traversals
+    // indicate if broken link
+    // this should work in similar fashion of the cities problem
+
+}
--- src/main/java/com/twoforboth/webcrawler/WebCrawler.java
+++ src/main/java/com/twoforboth/webcrawler/WebCrawler.java
@@ -4,6 +4,12 @@ public class WebCrawler {
 
     public static void main(String[] args) {
 
+        //Validate Arguments
+
+        //Instantiate processor object and process the url
+
+        //Generate Report
+
     }
 
 }
